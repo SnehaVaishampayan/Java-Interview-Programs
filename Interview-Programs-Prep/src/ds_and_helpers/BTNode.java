6d5daa721a8db74fb826d39545d9df703e11b838
sneha <sneha@SPGLenovo>
2018-02-03 06:32:46 -0800

    Adding DS Binary Tree Node and LinkedList Node

---
+++ Interview-Programs-Prep/src/ds_and_helpers/BTNode.java
@@ -0,0 +1,15 @@
+package ds_and_helpers;
+
+/*
+ *  Binary Tree node
+ */
+public class BTNode {
+
+	int data;
+	BTNode left;
+	BTNode right; 
+	
+	public BTNode(int data){
+		this.data = data;
+	}
+}
---
+++ Interview-Programs-Prep/src/ds_and_helpers/LLNode.java
@@ -0,0 +1,15 @@
+package ds_and_helpers;
+
+/*
+ *  Linked List Node
+ */
+
+public class LLNode {
+	int data;
+	LLNode next;
+	
+	public LLNode(int data){
+		this.data = data;
+	}
+
+}
